ea5e662d23de3fa3f84adecf600f7a938b1a0ee6
aruZeta <aru_hackZ.official@zohomail.eu>
2022-10-22 22:27:30 +0200

    [add] Different background color depending on current temperature

--- DarkSky/app/src/main/java/net/azarquiel/darksky/MainActivity.kt
+++ DarkSky/app/src/main/java/net/azarquiel/darksky/MainActivity.kt
@@ -13,13 +13,36 @@ import net.azarquiel.darksky.dao.DarkSky
 class MainActivity : AppCompatActivity() {
     private lateinit var data: DarkSky.Result
     private lateinit var mainView: ConstraintLayout
+    private var mainColor: Int = 0
 
     // I don't know much yet about how to handle coroutines, but seems like this works
     // and does not make android yell at you for blocking the main thread.
     private fun setup() = runBlocking {
         data = DarkSky.getForecast()
         mainView = findViewById(R.id.mainView)
+        mainColor = colorFromTemp(data.currently.temperature + 2.5F)
         Log.d("aru", data.currently.temperature.toString())
+    }
+
+    private fun colorFromTemp(temp: Float): Int {
+        // Took me a while to make this
+        fun Int.between(min: Int, max: Int): Int =
+            if (this > max) max
+            else if (this < min) min
+            else this
+        return Color.rgb(
+            ((temp + 30F) * 3.4F).toInt().between(0, 255),
+            (100F - temp).toInt().between(0, 100),
+            (255F - (temp * 3.4F)).toInt().between(0, 255)
+        )
+    }
+
+    private fun setGradientBackground() {
+        val bg: GradientDrawable = mainView.background as GradientDrawable
+        bg.colors = intArrayOf(
+            colorFromTemp(data.currently.temperature),
+            colorFromTemp(data.currently.temperature+5)
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +50,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setup()
+        setGradientBackground()
     }
 }
