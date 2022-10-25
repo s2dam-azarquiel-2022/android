62f6e67084f225a087fd4779a58973fee61610d2
aruZeta <aru_hackZ.official@zohomail.eu>
2022-10-25 12:28:29 +0200

    [add] Better TMDB DAO

--- TMDB/app/src/main/java/net/azarquiel/tmdb/dao/TMDB.kt
+++ TMDB/app/src/main/java/net/azarquiel/tmdb/dao/TMDB.kt
@@ -1,6 +1,7 @@
 package net.azarquiel.tmdb.dao
 
 import android.content.Context
+import android.util.Log
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import com.google.gson.Gson
@@ -21,14 +22,17 @@ object TMDB {
 
     data class Person(
         var name: String,
-        var popularity: Long,
+        var popularity: Float,
         @SerializedName("profile_path")
-        var image: String?
-    )
+        private var pfp: String?,
+        var id: Int
+    ) {
+        fun getPfp(): String? = if (pfp == null) null else "https://image.tmdb.org/t/p/w500/$pfp"
+    }
 
     @OptIn(DelicateCoroutinesApi::class)
     @Suppress("BlockingMethodInNonBlockingContext")
-    suspend fun getPeople(context: Context): Result {
+    suspend fun getPeople(): Result {
         var result: Result? = null
         GlobalScope.launch {
             val json = URL(url).readText(Charsets.UTF_8)
