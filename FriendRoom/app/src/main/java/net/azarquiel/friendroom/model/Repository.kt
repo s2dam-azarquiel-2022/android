b676e137086f934244d7204414884a2040d7c4a6
aruZeta <aru_hackZ.official@zohomail.eu>
2022-11-22 12:41:19 +0100

    [add] Repository model

---
+++ FriendRoom/app/src/main/java/net/azarquiel/friendroom/model/Repository.kt
@@ -0,0 +1,23 @@
+package net.azarquiel.friendroom.model
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+class ProductRepository(application: Application) {
+    val friendDAO = FriendsDB.getDB(application).friendDAO()
+    val familyDAO = FriendsDB.getDB(application).familyDAO()
+
+    fun getAllFriends(): LiveData<List<Friend>> = friendDAO.getAll()
+    fun getAllFamilies(): LiveData<List<Family>> = familyDAO.getAll()
+    fun getFriendById(id: Int): LiveData<List<Friend>> = friendDAO.getById(id)
+    fun getFamilyById(id: Int): LiveData<List<Family>> = familyDAO.getById(id)
+    fun addFriend(friend: Friend) = friendDAO.add(friend)
+    fun addFamily(family: Family) = familyDAO.add(family)
+    fun removeFriend(id: Int) = friendDAO.remove(id)
+    fun removeFamily(id: Int) = familyDAO.remove(id)
+    fun updateFriend(friend: Friend) = friendDAO.update(friend)
+    fun updateFamily(family: Family) = familyDAO.update(family)
+}
