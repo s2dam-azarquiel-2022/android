63a8696a8bc211dd310453e10c949b6586b87ac6
aruZeta <aru_hackZ.official@zohomail.eu>
2022-11-22 10:31:13 +0100

    [add] FriendRoom initial setup

---
+++ FriendRoom/app/src/main/java/net/azarquiel/friendroom/view/MainActivity.kt
@@ -0,0 +1,38 @@
+package net.azarquiel.friendroom.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import net.azarquiel.friendroom.R
+import net.azarquiel.friendroom.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private fun setup() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setup()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
