3a5fabb7425eed930b4725be407b5815d4828482
aruZeta <aru_hackZ.official@zohomail.eu>
2022-12-01 09:13:27 +0100

    [add] Group swipers

--- FriendRoom/app/src/main/java/net/azarquiel/friendroom/view/adapter/GroupAdapter.kt
+++ FriendRoom/app/src/main/java/net/azarquiel/friendroom/view/adapter/GroupAdapter.kt
@@ -1,25 +1,31 @@
 package net.azarquiel.friendroom.view.adapter
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import net.azarquiel.friendroom.R
 import net.azarquiel.friendroom.model.Group
+import net.azarquiel.friendroom.model.ProductSwipeHandler
 import net.azarquiel.friendroom.view.FriendActivity
 import net.azarquiel.friendroom.view.MainActivity
 import net.azarquiel.friendroom.viewModel.GroupViewModel
 
 class GroupAdapter(
     private val context: MainActivity,
-    thisView: RecyclerView,
+    private val thisView: RecyclerView,
     private val itemLayout: Int,
-    groupViewModel: GroupViewModel,
+    private val groupViewModel: GroupViewModel,
 ) : RecyclerView.Adapter<GroupAdapter.ViewHolder>() {
     private var data: List<Group> = emptyList()
     private val groupClickHandler = GroupClickHandler()
@@ -30,6 +36,8 @@ class GroupAdapter(
         groupViewModel.getAll().observe(context) { groups ->
             groups.let { this.setData(it) }
         }
+        ItemTouchHelper(GroupSwipeRightHandler()).attachToRecyclerView(thisView)
+        ItemTouchHelper(GroupSwipeLeftHandler()).attachToRecyclerView(thisView)
     }
 
     @SuppressLint("NotifyDataSetChanged")
@@ -67,6 +75,79 @@ class GroupAdapter(
                     it.putExtra("group", group)
                     context.startActivity(it)
                 }
+            }
+        }
+    }
+
+    inner class GroupSwipeRightHandler : ProductSwipeHandler(
+        ItemTouchHelper.LEFT,
+        ItemTouchHelper.RIGHT
+    ) {
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val group = data[viewHolder.adapterPosition]
+            groupViewModel.remove(group.id)
+            Snackbar.make(
+                thisView,
+                group.name,
+                Snackbar.LENGTH_LONG
+            ).setAction("Undo") {
+                groupViewModel.add(group)
+            }.show()
+        }
+    }
+
+    inner class GroupSwipeLeftHandler : ProductSwipeHandler(
+        ItemTouchHelper.RIGHT,
+        ItemTouchHelper.LEFT
+    ) {
+        @Suppress("NOTHING_TO_INLINE")
+        private inline fun View.setText(id: Int, text: String) {
+            this.findViewById<EditText>(id).setText(text)
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val pos = viewHolder.adapterPosition
+            val group = data[pos]
+            val dialogView = LayoutInflater.from(context).inflate(R.layout.new_group_alert, null)
+            dialogView.setText(R.id.newGroupName, group.name)
+            dialogView.setText(R.id.newGroupEmail, group.email)
+            AlertDialog.Builder(context)
+                .setTitle("Editar grupo")
+                .setView(dialogView)
+                .setPositiveButton(
+                    "Guardar",
+                    GroupEditHandler(dialogView, group)
+                )
+                .setNegativeButton(
+                    "Cancelar"
+                ) { _, _ -> this@GroupAdapter.notifyItemChanged(pos) }
+                .show()
+        }
+
+        inner class GroupEditHandler(
+            private val view: View,
+            private val group: Group,
+        ): DialogInterface.OnClickListener {
+            @Suppress("NOTHING_TO_INLINE")
+            private inline fun Int.getText(): String {
+                return view.findViewById<EditText>(this).text.toString()
+            }
+
+            override fun onClick(p0: DialogInterface?, p1: Int) {
+                groupViewModel.update(Group(
+                    group.id,
+                    (R.id.newGroupName).getText(),
+                    (R.id.newGroupEmail).getText(),
+                    group.color
+                ))
+
+                Snackbar.make(
+                    thisView,
+                    group.name,
+                    Snackbar.LENGTH_LONG
+                ).setAction("Undo") {
+                    groupViewModel.update(group)
+                }.show()
             }
         }
     }
