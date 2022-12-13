838b6110a4e294a727ab723aa9736a4dbb813a6d
aruZeta <aru_hackZ.official@zohomail.eu>
2022-12-13 17:07:30 +0100

    [add] Drawing BikeDetailedActivity

--- Alltricks/app/src/main/java/net/azarquiel/alltricks/view/BikeDetailedActivity.kt
+++ Alltricks/app/src/main/java/net/azarquiel/alltricks/view/BikeDetailedActivity.kt
@@ -3,6 +3,7 @@ package net.azarquiel.alltricks.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
 import net.azarquiel.alltricks.databinding.ActivityBikeDetailedBinding
 import net.azarquiel.alltricks.model.BikeDetailedView
 import net.azarquiel.alltricks.viewModel.BikeViewModel
@@ -11,7 +12,6 @@ class BikeDetailedActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBikeDetailedBinding
     private lateinit var bikeViewModel: BikeViewModel
-    private lateinit var bike: BikeDetailedView
 
     private fun setup() {
         binding = ActivityBikeDetailedBinding.inflate(layoutInflater)
@@ -19,9 +19,21 @@ class BikeDetailedActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         bikeViewModel = ViewModelProvider(this)[BikeViewModel::class.java]
+
         bikeViewModel.getById(intent.getSerializableExtra("bikeID") as Int).observe(this) {
-            bike = it[0]
+            drawBike(it[0])
         }
+    }
+
+    private fun drawBike(bike: BikeDetailedView) {
+        Picasso.get().load(bike.photo).into(binding.contentMain.bikePhoto)
+        binding.contentMain.brandName.text = bike.brand
+        binding.contentMain.bikeName.text = bike.description
+        binding.contentMain.bikePrice.text = bike.price
+        binding.contentMain.bikeStar.setImageResource(when (bike.favorite) {
+            1 -> android.R.drawable.star_on
+            else -> android.R.drawable.star_off
+        })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
