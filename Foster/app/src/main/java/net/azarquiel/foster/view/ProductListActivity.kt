f471d246c1e2c9cf981e988f19f9f0c233495ede
aruZeta <aru_hackZ.official@zohomail.eu>
2022-12-14 15:55:55 +0100

    [add] ProductListAdapter

--- Foster/app/src/main/java/net/azarquiel/foster/view/ProductListActivity.kt
+++ Foster/app/src/main/java/net/azarquiel/foster/view/ProductListActivity.kt
@@ -2,7 +2,11 @@ package net.azarquiel.foster.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import net.azarquiel.foster.R
 import net.azarquiel.foster.databinding.ActivityProductListBinding
+import net.azarquiel.foster.viemModel.ProductViewModel
+import net.azarquiel.foster.view.adapter.ProductAdapter
 
 class ProductListActivity : AppCompatActivity() {
 
@@ -12,6 +16,14 @@ class ProductListActivity : AppCompatActivity() {
         binding = ActivityProductListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        ProductAdapter(
+            this,
+            binding.content.recyclerProductList,
+            R.layout.product_row,
+            ViewModelProvider(this)[ProductViewModel::class.java],
+            intent.getSerializableExtra("categoryID") as Int
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ Foster/app/src/main/java/net/azarquiel/foster/view/adapter/ProductAdapter.kt
@@ -0,0 +1,82 @@
+package net.azarquiel.foster.view.adapter
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import net.azarquiel.foster.R
+import net.azarquiel.foster.model.ProductListView
+import net.azarquiel.foster.viemModel.ProductViewModel
+
+class ProductAdapter(
+    context: AppCompatActivity,
+    thisView: RecyclerView,
+    private val itemLayout: Int,
+    productViewModel: ProductViewModel,
+    categoryID: Int,
+) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+    private var data: List<ProductListView> = emptyList()
+    private val categoryClickHandler = BrandClickHandler()
+
+    init {
+        thisView.adapter = this
+        thisView.layoutManager = LinearLayoutManager(context)
+        productViewModel.getByCategoryID(categoryID).observe(context) { products ->
+            this.setData(products)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(data: List<ProductListView>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(LayoutInflater.from(parent.context).inflate(itemLayout, parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, pos: Int) = holder.bind(data[pos])
+    override fun getItemCount(): Int = data.size
+
+    inner class ViewHolder(layout: View) : RecyclerView.ViewHolder(layout) {
+        @Suppress("NOTHING_TO_INLINE")
+        private inline fun Int.setText(text: CharSequence) {
+            itemView.findViewById<TextView>(this).text = text
+        }
+
+        @Suppress("NOTHING_TO_INLINE")
+        private inline fun String?.checkNull(): String {
+            return this.let { if (it == "null") "" else it } ?: ""
+        }
+
+        fun bind(item: ProductListView) {
+            R.id.productTitle.setText(item.title.checkNull())
+            itemView.findViewById<ImageView>(R.id.productImage).let {
+                if (item.image.checkNull().isEmpty()) it.setImageResource(android.R.color.transparent)
+                else Picasso.get().load(item.image).into(it)
+            }
+            Log.d("aru", item.image.checkNull())
+
+            itemView.tag = item.id
+            itemView.setOnClickListener(categoryClickHandler)
+        }
+    }
+
+    inner class BrandClickHandler : View.OnClickListener {
+        override fun onClick(view: View?) {
+            (view?.tag as Int?).let { productID ->
+//                Intent(context, ProductDetailedActivity::class.java).let {
+//                    it.putExtra("productID", productID ?: -1)
+//                    context.startActivity(it)
+//                }
+            }
+        }
+    }
+}
