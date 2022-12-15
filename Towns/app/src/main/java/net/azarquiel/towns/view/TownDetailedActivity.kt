bfdd0c8c99a6acc6f96b11d4ec5960ca5ffffe23
aruZeta <aru_hackZ.official@zohomail.eu>
2022-12-15 10:30:03 +0100

    [add] Drawing TownDetailedActivity

--- Towns/app/src/main/java/net/azarquiel/towns/view/TownDetailedActivity.kt
+++ Towns/app/src/main/java/net/azarquiel/towns/view/TownDetailedActivity.kt
@@ -2,17 +2,29 @@ package net.azarquiel.towns.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import net.azarquiel.towns.databinding.ActivityTownDetailedBinding
+import net.azarquiel.towns.model.TownView
 
 class TownDetailedActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTownDetailedBinding
+    private lateinit var town: TownView
 
     private fun setup() {
         binding = ActivityTownDetailedBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
+        town = intent.getSerializableExtra("town") as TownView
+
+        Picasso.get().load(town.image).into(binding.content.townImage)
+        binding.content.townName.text = town.name
+        binding.content.townProvince.text = town.province
+
+        binding.content.moar.setOnClickListener {
+
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
