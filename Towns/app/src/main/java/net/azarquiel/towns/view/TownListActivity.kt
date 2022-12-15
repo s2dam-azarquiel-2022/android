b8c6721c49df01fdb36ac8f6600b608892807fc8
aruZeta <aru_hackZ.official@zohomail.eu>
2022-12-15 09:17:24 +0100

    [add] Get intent and set title

--- Towns/app/src/main/java/net/azarquiel/towns/view/TownListActivity.kt
+++ Towns/app/src/main/java/net/azarquiel/towns/view/TownListActivity.kt
@@ -3,15 +3,21 @@ package net.azarquiel.towns.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import net.azarquiel.towns.databinding.ActivityTownListBinding
+import net.azarquiel.towns.model.Community
 
 class TownListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTownListBinding
+    private lateinit var community: Community
 
     private fun setup() {
         binding = ActivityTownListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        community = intent.getSerializableExtra("community") as Community
+
+        title = community.name
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
