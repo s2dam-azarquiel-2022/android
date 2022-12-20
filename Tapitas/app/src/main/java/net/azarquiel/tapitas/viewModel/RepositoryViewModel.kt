de7970adc7189d22275be20cb90f3877b331fa6b
aruZeta <aru_hackZ.official@zohomail.eu>
2022-12-20 09:09:08 +0100

    [fix] Wrong naming

--- Tapitas/app/src/main/java/net/azarquiel/tapitas/viewModel/RepositoryViewModel.kt
+++ Tapitas/app/src/main/java/net/azarquiel/tapitas/viewModel/RepositoryViewModel.kt
@@ -7,12 +7,12 @@ import net.azarquiel.tapitas.model.StablishmentRepository
 import net.azarquiel.tapitas.model.TapaRepository
 import net.azarquiel.tapitas.model.TapaView
 
-class TapaRepository(application: Application) : AndroidViewModel(application) {
+class TapaViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = TapaRepository(application)
 
     fun getAll(): LiveData<List<TapaView>> = repository.getAll()
 }
 
-class StablishmentRepository(application: Application) {
+class StablishmentViewModel(application: Application) {
     private val repository = StablishmentRepository(application)
 }
