469565693e974ef6ea5bb7b0bd055896a2dea740
aruZeta <aru_hackZ.official@zohomail.eu>
2023-02-08 19:25:37 +0100

    [add] Painting MainActivity recycler

--- AvesRetrofit/app/src/main/java/net/azarquiel/avesretrofit/view/MainActivity.kt
+++ AvesRetrofit/app/src/main/java/net/azarquiel/avesretrofit/view/MainActivity.kt
@@ -1,11 +1,18 @@
 package net.azarquiel.avesretrofit.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import net.azarquiel.avesretrofit.R
 import net.azarquiel.avesretrofit.databinding.ActivityMainBinding
+import net.azarquiel.avesretrofit.databinding.ZoneRowBinding
+import net.azarquiel.avesretrofit.model.LiveAdapter
+import net.azarquiel.avesretrofit.model.Zone
+import net.azarquiel.avesretrofit.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,6 +21,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        val clickHandler = View.OnClickListener {
+            (it?.tag as Zone).let { zone ->
+                Intent(this, ZoneActivity::class.java).let { intent ->
+                    intent.putExtra("zone", zone)
+                    this.startActivity(intent)
+                }
+            }
+        }
+
+        LiveAdapter(
+            ViewModelProvider(this)[MainViewModel::class.java].getZones(),
+            this,
+            binding.content.recyclerZones,
+            ZoneRowBinding::inflate
+        ) { binding, view, item ->
+            binding.name.text = item.name
+            binding.localization.text = item.localization
+            view.tag = item
+            view.setOnClickListener(clickHandler)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
