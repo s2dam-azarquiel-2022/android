702fe17aae8ff81d58795938c7ef52cd2ff0d035
aruZeta <aru_hackZ.official@zohomail.eu>
2023-02-16 19:07:22 +0100

    [add] Getting passed province and setting title

--- Caravanas/app/src/main/java/net/azarquiel/caravanas/view/TownsActivity.kt
+++ Caravanas/app/src/main/java/net/azarquiel/caravanas/view/TownsActivity.kt
@@ -3,14 +3,19 @@ package net.azarquiel.caravanas.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import net.azarquiel.caravanas.databinding.ActivityTownsBinding
+import net.azarquiel.caravanas.model.Province
 
 class TownsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTownsBinding
+    private lateinit var province: Province
 
     private fun setup() {
         binding = ActivityTownsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        province = intent.extras?.get("province")!! as Province
+        title = "Municipios (${province.name})"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
