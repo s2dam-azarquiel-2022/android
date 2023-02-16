2586b1f2dfba29674c3392542a8fcab3624ce68b
aruZeta <aru_hackZ.official@zohomail.eu>
2023-02-16 19:44:42 +0100

    [add] Getting passed town and setting title

--- Caravanas/app/src/main/java/net/azarquiel/caravanas/view/ParkingsActivity.kt
+++ Caravanas/app/src/main/java/net/azarquiel/caravanas/view/ParkingsActivity.kt
@@ -3,14 +3,19 @@ package net.azarquiel.caravanas.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import net.azarquiel.caravanas.databinding.ActivityParkingsBinding
+import net.azarquiel.caravanas.model.Town
 
 class ParkingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityParkingsBinding
+    private lateinit var town: Town
 
     private fun setup() {
         binding = ActivityParkingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        town = intent.extras?.get("town")!! as Town
+        title = "Parkings (${town.name})"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
