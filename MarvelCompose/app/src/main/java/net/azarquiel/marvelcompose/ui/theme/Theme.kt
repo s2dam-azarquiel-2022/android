12e59320bcf1036bcb46f4c04eda0d175bf3b6d9
aruZeta <me@aruzeta.com>
2023-02-26 00:22:32 +0100

    [add] Colored status bar

--- MarvelCompose/app/src/main/java/net/azarquiel/marvelcompose/ui/theme/Theme.kt
+++ MarvelCompose/app/src/main/java/net/azarquiel/marvelcompose/ui/theme/Theme.kt
@@ -2,9 +2,12 @@ package net.azarquiel.marvelcompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -69,17 +72,30 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun AppTheme(
-  useDarkTheme: Boolean = isSystemInDarkTheme(),
-  content: @Composable () -> Unit
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
 ) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
+    val colors = if (!useDarkTheme) {
+        LightColors
+    } else {
+        DarkColors
+    }
 
-  MaterialTheme(
-    colorScheme = colors,
-    content = content
-  )
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = !isSystemInDarkTheme()
+    DisposableEffect(systemUiController, useDarkIcons) {
+        systemUiController.setSystemBarsColor(
+            color = colors.primary.darken(),
+            darkIcons = useDarkIcons,
+        )
+
+        onDispose { }
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
 }
+
+fun Color.darken() = this.copy(alpha = 0.9F)
